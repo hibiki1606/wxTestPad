9fe93c5fe7d6f87273a261c2f608774b913f687f
hibiki <158278526+verniy1606@users.noreply.github.com>
2025-02-23 07:51:00 +0900

    Change the Include

--- wxtestpad.cpp
+++ wxtestpad.cpp
@@ -1,5 +1,9 @@
 #include <wx/wxprec.h>
 
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+
 #include "./wxtestpad.h"
 #include "./gui/buildergen/wxTpGUIMainFrame.h"
 
--- wxtestpad.h
+++ wxtestpad.h
@@ -1,11 +1,6 @@
 #pragma once
-#include <wx/wxprec.h>
 
 #include <wx/txtstrm.h>
 #include <wx/wfstream.h>
-
-#ifndef WX_PRECOMP
-#include <wx/wx.h>
-#endif
 
 #define TP_PROJECT_NAME "wxTestPad"
